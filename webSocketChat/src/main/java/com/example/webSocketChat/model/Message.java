c218afa703fc534a7564124ccf37610fc8a4e09d
Felix Huang <fhuang726@gmail.com>
2019-10-08 23:15:07 -0700

    Add files via upload

---
+++ webSocketChat/src/main/java/com/example/webSocketChat/model/Message.java
@@ -0,0 +1,52 @@
+package com.example.webSocketChat.model;
+
+import com.alibaba.fastjson.JSON;
+
+public class Message {
+    public static final String ENTER = "ENTER";
+    public static final String SPEAK = "SPEAK";
+    public static final String QUIT = "QUIT";
+
+    private String username;
+    private String message;
+    private String type;
+    private int onlineCount;
+
+    public Message() {}
+
+    public Message(String username, String message, String type, int onlineCount) {
+        this.username = username;
+        this.message = message;
+        this.type = type;
+        this.onlineCount = onlineCount;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public static String jsonStr(String username, String msg, String type, int onlineTotal) {
+        return JSON.toJSONString(new Message(username, msg, type, onlineTotal));
+    }
+
+}
